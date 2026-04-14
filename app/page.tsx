8fdcc330794f4e097da71e95b526ea0e6627f6ea
Mathéo GUILBERT <matheo.guilbert49@gmail.com>
2026-04-14 20:08:37 +0200

    fix tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-beige text-black">
-      <h1>Hello World</h1>
+    <main className="min-h-screen bg-brand-beige text-brand-black flex items-center justify-center">
+      <h1 className="font-serif text-6xl">Hello World</h1>
     </main>
   );
 }
