a71f420afabf49834a6a98be273370fc3b4c3ce3
Matheo Guilbert <Matheo.Guilbert@ixapack.com>
2026-04-15 17:07:06 +0200

    responsive hero section

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -4,15 +4,17 @@ export default function HeroSection() {
 	return (
 		<section className="w-full h-[calc(100vh-(2*11px))] rounded-[10px] bg-brand-brown text-brand-beige flex flex-col items-center justify-center relative">
             {/* Title + image container */}
-            <div className="flex flex-row justify-between items-center w-2/3 gap-2.5">
+            <div className="flex flex-col w-9/10 gap-2.5
+                lg:flex-row lg:justify-between lg:items-center lg:w-2/3
+            ">
                 {/* Rotating texts */}
                 <div className="flex flex-col">
                     <p className="text-2xl font-sans">Ensemble nous allons...</p>
-                    <h1 className="text-[64px] font-serif">Construire une solution qui répond à votre problème.</h1>
+                    <h1 className="text-[clamp(30px,4vw,64px)] font-serif">Construire une solution qui répond à votre problème.</h1>
                 </div>
 
                 {/* Image */}
-                <img className="rounded-[10px]" src="/images/pofile.png" alt="Image of me" />
+                <img className="rounded-[10px] w-full h-[clamp(300px,50vh,700px)] object-cover" src="/images/pofile.png" alt="Image of me" />
             </div>
 
             {/* Call to scroll */}
