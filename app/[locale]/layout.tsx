f19535f4195d7f3c5c9df467f7ca0b674644b16a
Matheo Guilbert <Matheo.Guilbert@ixapack.com>
2026-04-17 13:16:07 +0200

    added vercel SpeedInsights package

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from 'next-intl/server';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./globals.css";
 
@@ -52,6 +53,8 @@ export default async function RootLayout({
 						<Footer />
 					</CustomScrollArea>
 				</NextIntlClientProvider>
+
+				<SpeedInsights />
 			</body>
 		</html>
 	);
